d80e07a4a1ac456d207e3dcc69d6f146d4376454
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-05-07 18:01:16 -0400

    Update 1a.java

--- 1a.java
+++ 1a.java
@@ -1,7 +1,7 @@
 /**
  * 2022 AP Computer Science Exam - Free Response Questions (FRQ)
  * Problems definition: https://apcentral.collegeboard.org/pdf/ap22-frq-computer-science-a.pdf
- * Solutions for question 1.a)
+ * Solutions for question 1(a)
  */
  
 public int getScore() {
