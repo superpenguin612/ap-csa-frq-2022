cef1e30c7764927e72bc57944c35d755ebdffd63
Florin Teodorescu <19256676+FlorinTeo@users.noreply.github.com>
2022-05-12 11:05:00 -0700

    Fix alternate 1.a), add alternate 4.b)
    Fixing minor bug in the alternate solution of 1.a)
    Adding an alternate solution for 4.b) for the edge case (unclear) of jagged grid[][].

--- 4b.java
+++ 4b.java
@@ -44,3 +44,57 @@ public int countIncreasingCols() {
     
     return count;
 }
+
+/** 
+ * Alternate solution covering the edge case when grid[][] is a non-rectangular (jagged) array.
+ * While jagged arrays are no longer part of the AP curricullum, students who know about them
+ * were tripped to look for a more complex solution like below.
+ *
+ * It would have been nice for the precondition of the method to state clearly 
+ * grid[][] is a rectangular double array.
+ */
+public int countIncreasingCols() {
+    
+    // Create an array list initially empty, where we
+    // accumulate the largest value encountered on each column.
+    // However if at any point we detect a value smaller than
+    // the largest value known already, the value for that column
+    // is set to null. And once is set to null it will not be tested
+    // or updated anymore.
+    ArrayList<Integer> colMaxValues = new ArrayList<Integer>();
+    
+    for (int r = 0; r < grid.length; r++) {
+        for (int c = 0; grid[r] != null && c < grid[r].length; c++) {
+            // if this is the first time we see column c, add the grid value into the list
+            if (colMaxValues.size() <= c) {
+                colMaxValues.add(grid[r][c]);
+            } else {
+                // we have some previous value for column c.
+                Integer colMaxValue = colMaxValues.get(c);
+                // if that value is null, it means we already determined c is not increasing
+                // but if it is not null ...
+                if (colMaxValue != null) {
+                    // ... check if the grid value is decreasing, case in which we know
+                    // column c is no longer increasing. So we set the value in the list to null.
+                    if (colMaxValue > grid[r][c]) {
+                        colMaxValues.set(c, null);
+                    } else {
+                        // otherwise we update the value in the list to the grid value, which is
+                        // the newer max for that column.
+                        colMaxValues.set(c, grid[r][c]);
+                    }
+                }
+            }
+        }
+    }
+    
+    // The number of non-null values in the colMaxValues gives the number of increasing columns.
+    int count = 0;
+    for(Integer colMaxValue : colMaxValues) {
+        if (colMaxValue != null) {
+            count++;
+        }
+    }
+    
+    return count;
+}
